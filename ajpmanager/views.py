6576e4eec32d75f6fb178897308f2be359acf877
Anton Vlasenko <antares.spica@gmail.com>
2012-10-02 20:09:51 +0300

    Improved permission model with moderators and administrators rights, new permission check function integrated in functions.

--- ajpmanager/views.py
+++ ajpmanager/views.py
@@ -1,12 +1,12 @@
 from pyramid.httpexceptions import HTTPForbidden
 from pyramid.view import view_config
 from pyramid.security import authenticated_userid, Allow, Authenticated, Everyone
+from ajpmanager.core.RedisAuth import groupfinder
 
 from ajpmanager.core.VMConnector import VMConnector
 
 VMC = VMConnector()
 dbcon = VMC.dbcon # Wrong way? Maybe this can lead to more slow work with DB
-
 
 class MainPage(object):
     """ Class for loading admin's menu view.
@@ -37,13 +37,43 @@ class MainPage(object):
         return {'username': username, 'show_settings': show_settings}
 
 
-
 class JSONprocessor(object):
 
     def __init__(self, request):
         self.request = request
         self.session = self.request.session.get_csrf_token()
         self.json = request.json_body
+
+    def __check_permissions(self, accepted_group, match_if_all=False):
+        """ A function to check user's permission in
+        internal factory functions. Accepts `accepted_group` parameter
+        must be without `group:` lead part.
+        Like: `admins`
+
+        """
+        username = authenticated_userid(self.request)
+        groups = groupfinder(username, self.request)
+
+        type_ = type(accepted_group)
+        if type_ is str: # single STR group
+            if 'group:' + accepted_group in groups:
+                return True
+            else:
+                return False
+        elif type_ is list: # multiple permissions
+            if match_if_all: # Match all
+                for item in accepted_group:
+                    if 'group:' + item not in groups:
+                        return False
+                return True
+            else:
+                for item in accepted_group:
+                    if 'group:' + item in groups:
+                        return True
+                return False
+        else:
+            raise TypeError ("Wrong accepted_groups type provided!")
+
 
     @view_config(renderer="json", route_name="engine.ajax", permission='registered')
     def mainline(self):
@@ -64,7 +94,6 @@ class JSONprocessor(object):
             print ('Wrong JSON request: ' + str(self.json['query']))
             return {'status': False, 'answer': 'Wrong query'}
 
-
     def get_vms_list(self):
         # Here we are reading all virtual machines and packing them into answer:
         no_cache = self.json.get('no_cache')
@@ -76,10 +105,14 @@ class JSONprocessor(object):
         return {'status': True, 'data': users}
 
     def get_groups_list(self):
+        if not self.__check_permissions(['admins', 'moderators']):
+            return {'status': False, 'answer': 'You are not authorized to get groups list'}
         groups = VMC.get_groups_list()
         return {'status': True, 'data': groups}
 
     def get_presets_list(self):
+        if not self.__check_permissions(['admins', 'moderators']):
+            return {'status': False, 'answer': 'You are not authorized to get presets list'}
         presets = VMC.get_presets_list()
         return {'status': True, 'data': presets}
 
@@ -88,6 +121,8 @@ class JSONprocessor(object):
         return {'status': True, 'answer': None}
 
     def verify_new_vm_name(self):
+        if not self.__check_permissions(['admins', 'moderators']):
+            return {'status': False, 'answer': 'You are not authorized to verify vms name'}
         name = self.json.get('data')
         if not name:
             return {'status': False, 'answer': 'No name provided'}
@@ -98,18 +133,25 @@ class JSONprocessor(object):
             return {'status': True}
 
     def get_storage_info(self):
+        if not self.__check_permissions(['admins', 'moderators']):
+            return {'status': False, 'answer': 'You are not authorized to get storage info'}
         machine = self.json.get('machine')
         if not machine:
             return {'status': False, 'answer': 'No machine name provided to calculate allocated storage'}
         total, used, free, preset_size = VMC.get_storage_info(machine)
         return {'status': True, 'data': {'free': free, 'used': used, 'total': total, 'preset_size': preset_size}}
 
+    @view_config(permission='admins')
     def get_settings(self):
+        if not self.__check_permissions('admins'):
+            return {'status': False, 'answer': 'You are not authorized to get settings info'}
         settings = VMC.get_settings()
         return {'status': True, 'data': settings}
 
 
     def apply_settings(self):
+        if not self.__check_permissions('admins'):
+            return {'status': False, 'answer': 'You are not authorized to get settings info'}
         data = self.json.get('data')
         if not data:
             return {'status': False, 'answer': 'No settings data provided'}
@@ -123,9 +165,10 @@ class JSONprocessor(object):
             return {'status': True}
 
     def restore_default_settings(self):
+        if not self.__check_permissions('admins'):
+            return {'status': False, 'answer': 'You are not authorized to get settings info'}
         VMC.restore_default_settings()
         return {'status': True}
-
 
     def machines_control(self):
         name = self.json.get('data')
@@ -163,10 +206,11 @@ class JSONprocessor(object):
         return {'status': answer}
 
     def add_user(self):
+        if not self.__check_permissions(['admins', 'moderators']):
+            return {'status': False, 'answer': 'You are not authorized to get settings info'}
         data = self.json['data']
         answer = VMC.add_user(data)
         return {'status': answer[0], 'answer': answer[1]}
-
 
     # This dictionary is used to implement factory run of the requested functions
     functions = {
